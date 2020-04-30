8645551163c6130d5377606e1c0bb544b454ea8b
Cameron Fischer <fischer.cameron@gmail.com>
2020-04-30 18:04:13 -0400

    Added processing instruction option

--- plugins/xml/parser.js
+++ plugins/xml/parser.js
@@ -13,13 +13,42 @@ The XML parser displays itself as xml.
 "use strict";
 
 var XmlParser = function(type,text,options) {
-	this.tree = [{
-		type: "codeblock",
-		attributes: {
-			code: {type: "string", value: text},
-			language: {type: "string", value: "xml"}
+	var template = getTemplate(text);
+	if (template) {
+		this.tree = [{
+			type: "transclude",
+			attributes: {
+				tiddler: {type: "string", value: template}
+			}
+		}];
+	} else {
+		this.tree = [{
+			type: "codeblock",
+			attributes: {
+				code: {type: "string", value: text},
+				language: {type: "string", value: "xml"}
+			}
+		}];
+	}
+};
+
+function getTemplate(text) {
+	var DOMParser = require("./xmldom").DOMParser;
+	var parser = new DOMParser();
+	var doc = parser.parseFromString(text, "text/xml");
+	var node = doc.firstChild;
+	while (node) {
+		if (node.target === "tiddlywiki") {
+			// TODO: This needs to be much more robust
+			var regexp = /template\s*=\s*['"]([^'"]*)['"]/
+			var match = node.data.match(regexp)
+			if (match) {
+				return match[1];
+			}
 		}
-	}];
+		node = node.nextSibling;
+	}
+	return undefined;
 };
 
 exports["text/xml"] = XmlParser;
--- tiddlers/test/parser.js
+++ tiddlers/test/parser.js
@@ -14,4 +14,14 @@ it('render xml tiddlers', function() {
 	expect(output).toContain('<dog name="Roofus">');
 });
 
+it('renders xml with tiddler template declaration', function() {
+	var wiki = new $tw.Wiki();
+	wiki.addTiddler({title: "test", type: "text/xml", text: '<?xml version="1.0" encoding="UTF-8"?>\n<?tiddlywiki template="myTemplate"?>\n<dogs><dog name="Roofus">Good dog</dog></dogs>'});
+	wiki.addTiddler({title: "myTemplate", text: 'A template file\n\n<$xsl for-each="/dogs/dog/@name"><<xmlNode>></$xsl>'});
+	var options = {variables: {currentTiddler: "test"}};
+	var output = wiki.renderTiddler("text/vnd.tiddlywiki", "test", options);
+	expect(output).toContain("A template file");
+	expect(output).toContain("Roofus");
+});
+
 });
