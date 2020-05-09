9f085315e1363365897bf20536cfb9497af19a45
Cameron Fischer <fischer.cameron@gmail.com>
2020-05-09 18:57:40 -0400

    Broke up XPathWidget into two classes

--- plugins/xml/widgets_xpath.js
+++ plugins/xml/widgets_xpath.js
@@ -16,22 +16,18 @@ var Widget = require("$:/core/modules/widgets/widget.js").widget;
 var xmlDom = require("../xmldom");
 var xpath = require("../xpath");
 
-var XPathWidget = function(parseTreeNode,options) {
-	this.initialise(parseTreeNode,options);
-};
+var DOMWidget = function() {};
 
-XPathWidget.prototype = new Widget();
+DOMWidget.prototype = new Widget();
 
-exports.xpath = XPathWidget;
-
-XPathWidget.prototype.render = function(parent,nextSibling) {
+DOMWidget.prototype.render = function(parent,nextSibling) {
 	this.parentDomNode = parent;
 	this.computeAttributes();
 	this.execute();
 	this.renderChildren(parent,nextSibling);
 };
 
-XPathWidget.prototype.execute = function() {
+DOMWidget.prototype.execute = function() {
 	this.template = this.getAttribute("template");
 	this.foreach = this.getAttribute("for-each");
 	this.valueof = this.getAttribute("value-of");
@@ -57,29 +53,27 @@ XPathWidget.prototype.execute = function() {
 		if (doc.error) {
 			members.push(this.makeErrorTree(doc.error));
 		} else {
-			var resolver = xpath.createResolver(contextNode, this);
 			if (this.foreach) {
 				var node = undefined;
 				try {
-					var iterator = xpath.evaluate(this.foreach, contextNode, resolver);
+					var iterator = this.queryAll(this.foreach, contextNode);
 					node = iterator.iterateNext();
 				} catch(e) {
-					var error = xpath.getError(e, this.foreach);
+					var error = this.handleQueryError(e, this.foreach);
 					members.push(this.makeErrorTree(error));
 				}
 				while (node) {
 					var value;
 					if (this.valueof) {
 						try {
-							var subiterator = xpath.evaluate(this.valueof, node, resolver);
-							var subnode = subiterator.iterateNext();
+							var subnode = this.query(this.valueof, node);
 							if (subnode) {
 								value = getStringValue(subnode);
 							} else {
 								value = "";
 							}
 						} catch(e) {
-							var error = xpath.getError(e, this.valueof);
+							var error = this.handleQueryError(e, this.valueof);
 							members.push(this.makeErrorTree(error));
 							break;
 						}
@@ -94,14 +88,13 @@ XPathWidget.prototype.execute = function() {
 				}
 			} else {
 				try {
-					var iterator = xpath.evaluate(this.valueof, contextNode, resolver);
-					node = iterator.iterateNext();
+					node = this.query(this.valueof, contextNode);
 					if (node) {
 						var value = getStringValue(node);
 						members.push(this.makeItemTemplate(null, value, false));
 					}
 				} catch (e) {
-					var error = xpath.getError(e, this.valueof);
+					var error = this.handleQueryError(e, this.valueof);
 					members.push(this.makeErrorTree(error));
 				}
 			}
@@ -116,9 +109,9 @@ function getStringValue(node) {
 		value = node.documentElement.textContent;
 	}
 	return value;
-};
+}
 
-XPathWidget.prototype.makeErrorTree = function(error) {
+DOMWidget.prototype.makeErrorTree = function(error) {
 	return {type: "element", tag: "span", attributes: {
 			"class": {type: "string", value: "tc-error"}
 		}, children: [
@@ -129,7 +122,7 @@ XPathWidget.prototype.makeErrorTree = function(error) {
 /*
 Compose the template for a list item
 */
-XPathWidget.prototype.makeItemTemplate = function(node, value, repeats) {
+DOMWidget.prototype.makeItemTemplate = function(node, value, repeats) {
 	var templateTree;
 	// Compose the transclusion of the template
 	if(this.template) {
@@ -150,14 +143,14 @@ XPathWidget.prototype.makeItemTemplate = function(node, value, repeats) {
 	}
 
 	// Return the list item
-	return {type: "xslnode", contextName: this.variableContext(), node: node, variableName: this.variableName, variableValue: value, children: templateTree};
+	return {type: "domnode", contextName: this.variableContext(), node: node, variableName: this.variableName, variableValue: value, children: templateTree};
 };
 
-XPathWidget.prototype.variableContext = function() {
+DOMWidget.prototype.variableContext = function() {
 	return "xml-node-state-" + this.xmlTitle;
 };
 
-XPathWidget.prototype.refresh = function(changedTiddlers) {
+DOMWidget.prototype.refresh = function(changedTiddlers) {
 	var changedAttributes = this.computeAttributes();
 	if(changedAttributes["for-each"] || changedAttributes["value-of"] || changedAttributes.variable || changedAttributes.tiddler || changedAttributes.template || changedTiddlers[this.xmlTitle] || namespacesChanged(changedAttributes)) {
 		this.refreshSelf();
@@ -180,13 +173,13 @@ function namespacesChanged(changedAttributes) {
 Widget exists for each loop of XPathWidget. Holds current state of
 that iteration.
 */
-var XslNodeWidget = function(parseTreeNode,options) {
+var DOMNodeWidget = function(parseTreeNode,options) {
 	this.initialise(parseTreeNode,options);
 };
 
-XslNodeWidget.prototype = new Widget();
+DOMNodeWidget.prototype = new Widget();
 
-XslNodeWidget.prototype.render = function(parent,nextSibling) {
+DOMNodeWidget.prototype.render = function(parent,nextSibling) {
 	this.parentDomNode = parent;
 	this.computeAttributes();
 	this.execute();
@@ -196,7 +189,7 @@ XslNodeWidget.prototype.render = function(parent,nextSibling) {
 /*
 Compute the internal state of the widget
 */
-XslNodeWidget.prototype.execute = function() {
+DOMNodeWidget.prototype.execute = function() {
 	// Set the current list item title
 	var node = this.parseTreeNode.node;
 	this.setVariable(this.parseTreeNode.variableName,this.parseTreeNode.variableValue);
@@ -211,10 +204,36 @@ XslNodeWidget.prototype.execute = function() {
 /*
 Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
 */
-XslNodeWidget.prototype.refresh = function(changedTiddlers) {
+DOMNodeWidget.prototype.refresh = function(changedTiddlers) {
 	return this.refreshChildren(changedTiddlers);
 };
 
-exports.xslnode = XslNodeWidget;
+exports.domnode = DOMNodeWidget;
+
+function XPathWidget(parseTreeNode, options) {
+	this.initialise(parseTreeNode,options);
+};
+
+XPathWidget.prototype = new DOMWidget();
+
+exports.xpath = XPathWidget;
+
+XPathWidget.prototype.queryAll = function(xpathQuery, contextNode) {
+	var resolver = xpath.createResolver(contextNode, this);
+	var iterator = xpath.evaluate(xpathQuery, contextNode, resolver);
+	return iterator;
+};
+
+XPathWidget.prototype.query = function(xpathQuery, contextNode) {
+	var resolver = xpath.createResolver(contextNode, this);
+	var iterator = xpath.evaluate(xpathQuery, contextNode, resolver);
+	return iterator.iterateNext();
+};
+
+XPathWidget.prototype.handleQueryError = function(error, offendingQuery) {
+	return xpath.getError(error, offendingQuery);
+};
+
+//return {nodeList: nodeList, index = 0, iterateNext = function() { return this.nodeList[this.index++] }};
 
 })();
